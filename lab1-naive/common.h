5eb3be705fc6810b1e398a1dc2079465d857a591
FluorineDog <fluorine@gmail.com>
2018-07-12 23:25:49 +0800

    baseline_done

--- lab1-naive/common.h
+++ lab1-naive/common.h
@@ -3,20 +3,36 @@
 #include <functional>
 #include <iostream>
 #include <memory>
+#include <random>
 #include <string>
 using namespace std::chrono;
 typedef void functor_t(size_t size, double A[], double B[], double C[]);
 
 void execute(std::string name, std::function<functor_t> func) {
-  constexpr int SIZE = 100;
+  std::default_random_engine e;
+  std::uniform_real_distribution<> dist(-1.0, 1.0);
+  constexpr size_t SIZE = 1 << 25;
+  constexpr size_t REPEAT = 10;
   // std::array<double, SIZE> A, B, C;
   auto A = std::make_unique<double[]>(SIZE);
   auto B = std::make_unique<double[]>(SIZE);
   auto C = std::make_unique<double[]>(SIZE);
-
+  for (size_t i = 0; i < SIZE; ++i) {
+    A[i] = dist(e);
+    B[i] = dist(e);
+  }
   auto beg_time = high_resolution_clock::now();
-  func(SIZE, A.get(), B.get(), C.get());
+  for(size_t i = 0; i < REPEAT; ++i){
+    func(SIZE, A.get(), B.get(), C.get());
+    func(SIZE, B.get(), C.get(), A.get());
+    func(SIZE, C.get(), A.get(), B.get());
+  }
   auto end_time = high_resolution_clock::now();
+  { 
+    // never executable
+    double value = C[e() % SIZE];
+    if (value > 1.0e100) exit(-1);
+  }
   auto time =
       duration_cast<duration<double, std::milli>>(end_time - beg_time).count();
   std::cout << name << ":\t" << time << " ms" << std::endl;
