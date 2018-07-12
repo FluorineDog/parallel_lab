f5223f5c2db5a5a1bbf550a1fc5c5677702bc909
FluorineDog <fluorine@gmail.com>
2018-07-12 23:27:55 +0800

    init

--- lab1-naive/benchmark.cpp
+++ lab1-naive/benchmark.cpp
@@ -6,23 +6,7 @@ int baseline_add(size_t size, double A[], double B[], double C[]) {
   }
 }
 
-#include <thread>
-#include <vector>
-int pthread_add(size_t size, double A[], double B[], double C[]) {
-  std::vector<std::thread> threads;
-  int thread_num = std::thread::hardware_concurrency();
-  for (int t_id = 0; t_id < thread_num; ++t_id) {
-    size_t beg = (t_id * size) / thread_num;
-    size_t end = (t_id + 1) * size / thread_num;
-    threads.emplace_back(
-        [](size_t beg, size_t end, double A[], double B[], double C[]) {
-          for (size_t i = beg; i < end; ++i) {
-            C[i] = B[i] + A[i];
-          }
-        },
-        beg, end, A, B, C);
-  }
-}
+
 
 int openmp_add(size_t size, double A[], double B[], double C[]) {
 #pragma omp parallel for
---
+++ lab1-naive/pthread-add.cpp
@@ -0,0 +1,23 @@
+#include <thread>
+#include <vector>
+int pthread_add(size_t size, double A[], double B[], double C[]) {
+  std::vector<std::thread> threads;
+  int thread_num = std::thread::hardware_concurrency();
+  for (int t_id = 0; t_id < thread_num; ++t_id) {
+    size_t beg = (t_id * size) / thread_num;
+    size_t end = (t_id + 1) * size / thread_num;
+    threads.emplace_back(
+        [](size_t beg, size_t end, double A[], double B[], double C[]) {
+          for (size_t i = beg; i < end; ++i) {
+            C[i] = B[i] + A[i];
+          }
+        },
+        beg, end, A, B, C);
+  }
+}
+
+#include "common.h"
+int main(){
+  EXEC(pthread_add);
+  return 0;
+}
