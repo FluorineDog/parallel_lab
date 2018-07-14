d1a4af2f7a1159b8ea5c6f66de12b669dfad887d
FluorineDog <fluorine@gmail.com>
2018-07-15 02:36:16 +0800

    pthread done

--- lab2-pthread/pthread-erode.cpp
+++ lab2-pthread/pthread-erode.cpp
@@ -2,8 +2,9 @@
 #include <vector>
 #include "../common/opencv-bench.h"
 
-void erode_pthread(Mat& src, Mat& dst, Mat& kernel) {
-  for (int base_row = 0; base_row < src.rows; ++base_row) {
+void erode_pthread_workload(Mat& src, Mat& dst, Mat& kernel, int beg_r,
+                            int end_r) {
+  for (int base_row = beg_r; base_row < end_r; ++base_row) {
     for (int base_col = 0; base_col < src.cols; ++base_col) {
       int len_row = std::min(kernel.rows, src.rows - base_row);
       int len_col = std::min(kernel.cols, src.cols - base_col);
@@ -23,11 +24,29 @@ void erode_pthread(Mat& src, Mat& dst, Mat& kernel) {
   }
 }
 
-
-void erode_ref(Mat& src, Mat& dst, Mat& kernel) {
-  erode(src, dst, kernel);
+void erode_pthread(Mat& src, Mat& dst, Mat& kernel) {
+  std::vector<std::thread> threads;
+  int thread_num = std::thread::hardware_concurrency();
+  // split by rows
+  int size = src.rows;
+  
+  for (int t_id = 0; t_id < thread_num; ++t_id) {
+    size_t beg = (t_id * size) / thread_num;
+    size_t end = (t_id + 1) * size / thread_num;
+    if (t_id != thread_num - 1) {
+      threads.emplace_back(
+          [&, beg, end] { erode_pthread_workload(src, dst, kernel, beg, end); });
+    } else {
+      // the last thread is the main thread itself
+      erode_pthread_workload(src, dst, kernel, beg, end);
+    }
+  }
+  for (auto& th : threads) {
+    th.join();
+  }
 }
 
+void erode_ref(Mat& src, Mat& dst, Mat& kernel) { erode(src, dst, kernel); }
 
 int main() {
   EXEC_CV(erode_pthread);
