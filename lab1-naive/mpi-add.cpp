41846f05a82f1e2f149a589a77bc8f52a4b144aa
Guilin Gou <guilin.gou@zilliz.com>
2018-07-14 09:02:55 +0800

    stupid_dog

--- lab1-naive/mpi-add.cpp
+++ lab1-naive/mpi-add.cpp
@@ -1,10 +1,17 @@
 #include "common.h"
+#include <mpi.h>
 
 void mpi_add(size_t size, double A[], double B[], double C[]) {
-
+  cout << "hello, world" << endl;
+  // for (size_t i = 0; i < size; ++i) {
+  //   C[i] = B[i] + A[i];
+  // }
 }
 
+
 int main() {
-  MPI_INIT()
-	EXEC(mpi_add);
+  MPI_Init(nullptr, nullptr);
+  EXEC(mpi_add);
+  MPI_Finalize();
+  return 0;
 }
