37ddee0d03f1f59ed91579db3b4b1fcc28721e36
FluorineDog <fluorine@gmail.com>
2018-07-15 07:09:05 +0800

    up

--- lab3-mpi/mpi-erode.cpp
+++ lab3-mpi/mpi-erode.cpp
@@ -2,27 +2,34 @@
 #include <vector>
 #include "../common/opencv-bench.h"
 
-void erode_pthread(Mat& src, Mat& dst, Mat& kernel) {
-  for (int base_row = 0; base_row < src.rows; ++base_row) {
-    for (int base_col = 0; base_col < src.cols; ++base_col) {
-      int len_row = std::min(kernel.rows, src.rows - base_row);
-      int len_col = std::min(kernel.cols, src.cols - base_col);
-      uint8_t pixel = 255;
-      auto anchor = src.data + base_row * src.cols + base_col;
-#pragma unroll(10)
-      for (int ki = 0; ki < len_row; ++ki) {
-#pragma unroll(10)
-        for (int kj = 0; kj < len_col; ++kj) {
-          if (kernel.data[ki * base_col + kj]) {
-            pixel = std::min(pixel, anchor[ki * src.cols + kj]);
-          }
-        }
-      }
-      dst.data[base_row * src.cols + base_col] = pixel;
-    }
-  }
+void erode_pthread_workload(uint8_t* src, uint8_t* dst,uint8_t* kernel, 
+int beg_r, int end_r, int cols, int COL_STRIDE
+) {
+//   for (int base_row = 0; base_row < src.rows; ++base_row) {
+//     for (int base_col = 0; base_col < src.cols; ++base_col) {
+//       int len_row = std::min(kernel.rows, src.rows - base_row);
+//       int len_col = std::min(kernel.cols, src.cols - base_col);
+//       uint8_t pixel = 255;
+//       auto anchor = src.data + base_row * src.cols + base_col;
+// #pragma unroll(10)
+//       for (int ki = 0; ki < len_row; ++ki) {
+// #pragma unroll(10)
+//         for (int kj = 0; kj < len_col; ++kj) {
+//           if (kernel.data[ki * base_col + kj]) {
+//             pixel = std::min(pixel, anchor[ki * src.cols + kj]);
+//           }
+//         }
+//       }
+//       dst.data[base_row * src.cols + base_col] = pixel;
+//     }
+//   }
 }
 
+void erode_pthread_workload(Mat& src, Mat& dst, Mat& kernel) {
+  int rows = src.rows; 
+  int cols = src.cols;
+  // MPI_Scatter()
+}
 
 void erode_ref(Mat& src, Mat& dst, Mat& kernel) {
   erode(src, dst, kernel);
