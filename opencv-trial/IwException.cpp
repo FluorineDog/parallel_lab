97e5847bbbce3814bd13a8cead926e4467e318c3
Guilin Gou <guilin.gou@zilliz.com>
2018-07-14 21:51:17 +0800

    baseline

--- opencv-trial/IwException.cpp
+++ opencv-trial/IwException.cpp
@@ -1,32 +1,27 @@
 #include <iostream>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
+#include <exception>
 using namespace cv;
 using namespace std;
 int main() {
-  const int KernelWidth = 10;
-  const int TileWidth = 10;
-  Mat srcImage = imread("data/lena.png", CV_LOAD_IMAGE_GRAYSCALE);
-  try {
-    imshow("original picture", srcImage);
-  } catch (cv::Exception& ex) {
-    fprintf(stderr, "Exception converting image to PNG format: %s\n",
-            ex.what());
-    getchar();
-    return -1;
+	const int KernelWidth = 10;
+	const int TileWidth = 10;
+	Mat dst_image;
+	Mat src_image = imread("data/lena.png", CV_LOAD_IMAGE_GRAYSCALE);
+	try {
+		imshow("original picture", src_image);
+		cout << "cols:" << src_image.cols << " rows:" << src_image.rows << endl;
+		Mat element = getStructuringElement(MORPH_RECT, Size(10, 10));
+		erode(src_image, dst_image, element);
+	  imshow("eroded picture all", dst_image);
+    waitKey(0);
+	} catch (cv::Exception& ex) {
+		cout << "cv Exception:" << ex.what();
+		return -1;
+	} catch(std::exception& ex) {
+		cout << "std Exception:" << ex.what();
+    return -2;
   }
-  cout << "cols:" << srcImage.cols << " rows:" << srcImage.rows << endl;
-  Mat element = getStructuringElement(MORPH_RECT, Size(10, 10));
-  Mat dstImage;
-  try {
-    erode(srcImage, dstImage, element);
-  } catch (cv::Exception& ex) {
-    cout << "Exception:" << ex.what();
-    getchar();
-    return -1;
-  }
-  imshow("eroded picture all", dstImage);
-  waitKey(0);
-  getchar();
-  return 0;
+	return 0;
 }
