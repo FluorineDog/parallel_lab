8f2932d79d49a9a1ebd6bac1b6ff7c053e6b87ed
Guilin Gou <guilin.gou@zilliz.com>
2018-07-14 10:04:37 +0800

    update

--- lab1-naive/common.h
+++ lab1-naive/common.h
@@ -1,10 +1,12 @@
+#include <algorithm>
 #include <array>
+#include <boost/functional/hash.hpp>
 #include <chrono>
 #include <functional>
+#include <iostream>
 #include <memory>
 #include <random>
 #include <string>
-#include <iostream>
 using std::cin;
 using std::cout;
 using std::endl;
@@ -12,43 +14,47 @@ using std::endl;
 using namespace std::chrono;
 typedef void functor_t(size_t size, double A[], double B[], double C[]);
 
+constexpr size_t SIZE = 1 << 25;
 void execute(std::string name, std::function<functor_t> func) {
-  std::default_random_engine e;
-  std::uniform_real_distribution<> dist(-1.0, 1.0);
-  constexpr size_t SIZE = 1 << 27;
-  constexpr size_t REPEAT = 5;
-  // std::array<double, SIZE> A, B, C;
-  auto A = std::make_unique<double[]>(SIZE);
-  auto B = std::make_unique<double[]>(SIZE);
-  auto C = std::make_unique<double[]>(SIZE);
-  #pragma omp parallel for
-  for (size_t i = 0; i < SIZE; ++i) {
-    A[i] = 1.0 * i / SIZE;
-    B[i] = 0.9 * i / SIZE;
-  }
+	std::default_random_engine e;
+	std::uniform_real_distribution<> dist(-1.0, 1.0);
+	constexpr size_t REPEAT = 5;
+	// std::array<double, SIZE> A, B, C;
+	auto A = std::make_unique<double[]>(SIZE);
+	auto B = std::make_unique<double[]>(SIZE);
+	auto C = std::make_unique<double[]>(SIZE);
+#pragma omp parallel for
+	for (size_t i = 0; i < SIZE; ++i) {
+		A[i] = 1.0 * i / SIZE;
+		B[i] = 0.9 * i / SIZE;
+	}
 
-  // warm up
-  for(size_t i = 0; i < REPEAT / 2; ++i){
-    func(SIZE, A.get(), B.get(), C.get());
-    func(SIZE, B.get(), C.get(), A.get());
-    func(SIZE, C.get(), A.get(), B.get());
-  }
-  auto beg_time = high_resolution_clock::now();
-  for(size_t i = 0; i < REPEAT; ++i){
-    func(SIZE, A.get(), B.get(), C.get());
-    func(SIZE, B.get(), C.get(), A.get());
-    func(SIZE, C.get(), A.get(), B.get());
-  }
-  auto end_time = high_resolution_clock::now();
-  { 
-    // never executable
-    double value = C[e() % SIZE];
-    if (value > 1.0e100) exit(-1);
-  }
-  auto time =
-      duration_cast<duration<double, std::milli>>(end_time - beg_time).count();
-  time /= 30;
-  std::cout << name << ":\t" << time << " ms" << std::endl;
+	// warm up
+	for (size_t i = 0; i < REPEAT / 2; ++i) {
+		func(SIZE, A.get(), B.get(), C.get());
+		func(SIZE, B.get(), C.get(), A.get());
+		func(SIZE, C.get(), A.get(), B.get());
+	}
+	auto beg_time = high_resolution_clock::now();
+	for (size_t i = 0; i < REPEAT; ++i) {
+		func(SIZE, A.get(), B.get(), C.get());
+		func(SIZE, B.get(), C.get(), A.get());
+		func(SIZE, C.get(), A.get(), B.get());
+	}
+	auto end_time = high_resolution_clock::now();
+	{
+		// never executable
+		double value = C[e() % SIZE];
+		if (value > 1.0e100) exit(-1);
+	}
+	auto time =
+			duration_cast<duration<double, std::milli>>(end_time - beg_time).count();
+	time /= 30;
+
+	std::cout << name << ":\t" << time << " ms";
+	auto signature = boost::hash_range(B.get(), B.get() + SIZE);
+	cout << " " << std::hex  << signature;
+	cout << std::endl;
 }
 
 #define EXEC(functor) execute(#functor, functor)
--- lab1-naive/mpi-add.cpp
+++ lab1-naive/mpi-add.cpp
@@ -1,17 +1,70 @@
-#include "common.h"
 #include <mpi.h>
-
+#include "common.h"
+auto buffer_a = std::make_unique<double[]>(1 << 25);
+auto buffer_b = std::make_unique<double[]>(1 << 25);
+auto buffer_c = std::make_unique<double[]>(1 << 25);
 void mpi_add(size_t size, double A[], double B[], double C[]) {
-  cout << "hello, world" << endl;
-  // for (size_t i = 0; i < size; ++i) {
-  //   C[i] = B[i] + A[i];
-  // }
+	assert(size <= (1 << 27));
+  MPI_Bcast(&size, 1, MPI_UNSIGNED_LONG_LONG, 0, MPI_COMM_WORLD);
+  cerr << size;
+	int concurrency;
+	int rank;
+	MPI_Comm_size(MPI_COMM_WORLD, &concurrency);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	size_t beg = size * rank / concurrency;
+	size_t end = size * (rank + 1) / concurrency;
+	MPI_Scatter(A, size, MPI_DOUBLE, buffer_a.get(), end - beg, MPI_DOUBLE, 0,
+							MPI_COMM_WORLD);
+	MPI_Scatter(B, size, MPI_DOUBLE, buffer_b.get(), end - beg, MPI_DOUBLE, 0,
+							MPI_COMM_WORLD);
+	for (size_t i = beg; i < end; ++i) {
+		buffer_c[i] = buffer_b[i] + buffer_a[i];
+	}
+
+	MPI_Gather(buffer_c.get(), end - beg, MPI_DOUBLE, C, size, MPI_DOUBLE, 0,
+						 MPI_COMM_WORLD);
 }
 
+void mpi_slave() {
+
+	while (true) {
+    size_t size;
+    MPI_Bcast(&size, 1, MPI_UNSIGNED_LONG_LONG, 0, MPI_COMM_WORLD);
+    cerr << size;
+
+		int concurrency;
+		int rank;
+		MPI_Comm_size(MPI_COMM_WORLD, &concurrency);
+		MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+		size_t beg = size * rank / concurrency;
+		size_t end = size * (rank + 1) / concurrency;
+
+		MPI_Scatter(nullptr, size, MPI_DOUBLE, buffer_a.get(), end - beg,
+								MPI_DOUBLE, 0, MPI_COMM_WORLD);
+		MPI_Scatter(nullptr, size, MPI_DOUBLE, buffer_b.get(), end - beg,
+								MPI_DOUBLE, 0, MPI_COMM_WORLD);
+		for (size_t i = beg; i < end; ++i) {
+			buffer_c[i] = buffer_b[i] + buffer_a[i];
+		}
+
+		MPI_Gather(buffer_c.get(), end - beg, MPI_DOUBLE, nullptr, size, MPI_DOUBLE,
+							 0, MPI_COMM_WORLD);
+	}
+}
 
 int main() {
-  MPI_Init(nullptr, nullptr);
-  EXEC(mpi_add);
-  MPI_Finalize();
-  return 0;
+	MPI_Init(nullptr, nullptr);
+
+	int rank;
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+	if (rank == 0) {
+		MPI_Barrier(MPI_COMM_WORLD);
+		EXEC(mpi_add);
+	} else {
+		MPI_Barrier(MPI_COMM_WORLD);
+		mpi_slave();
+	}
+	MPI_Finalize();
+	return 0;
 }
