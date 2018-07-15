7a8d8a4dfe9be9de179d5f23cc73a923ad676661
Guilin Gou <guilin.gou@zilliz.com>
2018-07-15 09:03:19 +0800

    buggy

--- lab4-mpi/mpi-erode.cpp
+++ lab4-mpi/mpi-erode.cpp
@@ -15,24 +15,24 @@ void erode_workload(uint8_t *src, uint8_t *dst, uint8_t *kernel,
                     int beg_r, int end_r,
                     const Config &config
 ) {
-//   for (int base_row = 0; base_row < src.rows; ++base_row) {
-//     for (int base_col = 0; base_col < src.cols; ++base_col) {
-//       int len_row = std::min(kernel.rows, src.rows - base_row);
-//       int len_col = std::min(kernel.cols, src.cols - base_col);
-//       uint8_t pixel = 255;
-//       auto anchor = src.data + base_row * src.cols + base_col;
-// #pragma unroll(10)
-//       for (int ki = 0; ki < len_row; ++ki) {
-// #pragma unroll(10)
-//         for (int kj = 0; kj < len_col; ++kj) {
-//           if (kernel.data[ki * base_col + kj]) {
-//             pixel = std::min(pixel, anchor[ki * src.cols + kj]);
-//           }
-//         }
-//       }
-//       dst.data[base_row * src.cols + base_col] = pixel;
-//     }
-//   }
+  for (int base_row = 0; base_row < config.rows; ++base_row) {
+    for (int base_col = 0; base_col < config.cols; ++base_col) {
+      int len_row = std::min(config.kernel_rows, config.rows - base_row);
+      int len_col = std::min(config.kernel_cols, config.cols - base_col);
+      uint8_t pixel = 255;
+      auto anchor = src + base_row * config.cols + base_col;
+#pragma unroll(10)
+      for (int ki = 0; ki < len_row; ++ki) {
+#pragma unroll(10)
+        for (int kj = 0; kj < len_col; ++kj) {
+          if (kernel[ki * base_col + kj]) {
+            pixel = std::min(pixel, anchor[ki * config.cols + kj]);
+          }
+        }
+      }
+      dst[base_row * config.cols + base_col] = pixel;
+    }
+  }
 }
 
 
