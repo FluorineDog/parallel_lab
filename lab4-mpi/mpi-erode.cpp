92bd27594608c4b71f600b5628b4436881f00e94
Guilin Gou <guilin.gou@zilliz.com>
2018-07-15 08:39:15 +0800

    mpi-half

--- lab4-mpi/mpi-erode.cpp
+++ lab4-mpi/mpi-erode.cpp
@@ -4,10 +4,16 @@
 #include <memory>
 #include <mpi.h>
 
+struct Config {
+    int rows;
+    int cols;
+    int kernel_rows;
+    int kernel_cols;
+};
 
 void erode_workload(uint8_t *src, uint8_t *dst, uint8_t *kernel,
-                    int beg_r, int end_r, int cols
-                    int kernel_rows, int kernel_cols
+                    int beg_r, int end_r,
+                    const Config& config
 ) {
 //   for (int base_row = 0; base_row < src.rows; ++base_row) {
 //     for (int base_col = 0; base_col < src.cols; ++base_col) {
@@ -29,14 +35,36 @@ void erode_workload(uint8_t *src, uint8_t *dst, uint8_t *kernel,
 //   }
 }
 
-struct Config {
-    int cols;
-    int kernel_rows;
-    int kernel_cols;
-};
 
 
-void erode_pthread(Mat &src, Mat &dst, Mat &kernel) {
+void erode_slave() {
+  MPI_Request handle;
+  int concurrency;
+  int rank;
+  MPI_Comm_size(MPI_COMM_WORLD, &concurrency);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  Config config;
+  MPI_Bcast(&config, 5, MPI_INT, 0, MPI_COMM_WORLD);
+  int rows = config.rows;
+  int cols = config.cols;
+  int kernel_area = config.kernel_rows * config.kernel_cols;
+  auto kernel = std::make_unique<uint8_t[]>(kernel_area);
+  MPI_Bcast(kernel.get(), kernel_area, MPI_UINT8_T, 0, MPI_COMM_WORLD);
+  
+  
+  const int beg = rank * rows / concurrency;
+  const int end = (rank + 1) * rows / concurrency;
+  const int end_padded = std::min(end + 10, rows);
+  int src_area = (end_padded - beg) * cols;
+  int dst_area = (end - beg) * cols;
+  auto buffer = std::make_unique<uint8_t[]>(src_area);
+  auto dst_buffer = std::make_unique<uint8_t[]>(dst_area);
+  MPI_Recv(buffer.get(), src_area, MPI_UINT8_T, 0, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+  erode_workload(buffer.get(), dst_buffer.get(), kernel.get(), beg, end, config);
+  MPI_Gatherv(dst_buffer.get(), dst_area, MPI_INT8_T, nullptr, nullptr, nullptr, MPI_UINT8_T, 0, MPI_COMM_WORLD);
+}
+
+void erode_mpi(Mat &src, Mat &dst, Mat &kernel) {
   int rows = src.rows;
   int cols = src.cols;
   MPI_Request handle;
@@ -45,29 +73,36 @@ void erode_pthread(Mat &src, Mat &dst, Mat &kernel) {
   int rank;
   MPI_Comm_size(MPI_COMM_WORLD, &concurrency);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  assert(rank == 0);
   
   Config config;
+  config.rows = src.rows;
   config.cols = src.cols;
   config.kernel_rows = kernel.rows;
   config.kernel_cols = kernel.cols;
   
-  MPI_Isend(&config, 5, MPI_INT, tid, 0, MPI_COMM_WORLD, &handle);
-  MPI_Request_free(&handle);
-  for (int tid = 0; tid < concurrency - 1; ++tid) {
+  MPI_Bcast(&config, 5, MPI_INT, 0, MPI_COMM_WORLD);
+  MPI_Bcast(kernel.data, config.kernel_rows * config.kernel_cols, MPI_UINT8_T, 0, MPI_COMM_WORLD);
+  std::vector<int> recvcounts;
+  std::vector<int> displs;
+  recvcounts.push_back(0);
+  displs.push_back(0);
+  for (int tid = 1; tid < concurrency; ++tid) {
     // split by line
     int beg = tid * rows / concurrency;
     int end = (tid + 1) * rows / concurrency;
+    displs.push_back(beg);
+    recvcounts.push_back((end - beg) * cols);
     end = std::min(end + 10, rows);
-    
-    
     MPI_Isend(src.data, (end - beg) * cols, MPI_UINT8_T, tid, 1, MPI_COMM_WORLD, &handle);
     MPI_Request_free(&handle);
   }
-  int tid = concurrency - 1;
+  int tid = 0;
   int beg = tid * rows / concurrency;
   int end = (tid + 1) * rows / concurrency;
-  
-  erode_workload(src.data, dst.data, kernel.data, beg, end, src.cols, kernel.rows, kernel.cols);
+  erode_workload(src.data, dst.data, kernel.data, beg, end, config);
+  MPI_Gatherv(MPI_IN_PLACE, (end - beg) * cols, MPI_UINT8_T, src.data,
+              recvcounts.data(), displs.data(), MPI_UINT8_T, 0, MPI_COMM_WORLD);
 }
 
 void erode_ref(Mat &src, Mat &dst, Mat &kernel) {
@@ -76,6 +111,11 @@ void erode_ref(Mat &src, Mat &dst, Mat &kernel) {
 
 
 int main() {
-  EXEC_CV(erode_pthread);
+  int rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  if(rank == 0){
+    EXEC_CV(erode_mpi);
+  }
+  
   return 0;
 }
